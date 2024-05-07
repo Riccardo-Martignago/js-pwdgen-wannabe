ea60747a1e055cbd067bd41b3a25f1a0006fbfcb
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-05-07 19:26:50 +0200

    Creato file html e js con contenuto richiesto

---
+++ JS/script.js
@@ -0,0 +1,4 @@
+const userName = prompt('Scrivi il tuo nome')
+const userSurname = prompt('Scrivi il tuo cognome')
+const userColor = prompt('Scrivi il tuo colore preferito')
+document.getElementById('password').innerHTML = userName + userSurname + userColor;
